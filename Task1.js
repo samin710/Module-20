ab5f6ef7fa4383bac8b3f70df2997d67c90f7dc0
samin710 <smsaminii112@gmail.com>
2025-02-11 21:33:11 +0600

    Task1 done

---
+++ Task1.js
@@ -0,0 +1,12 @@
+// Access the golden rod color value in output.
+
+const colors = {
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+  "golden red": "#daa520",
+};
+
+console.log(colors.blue);
+console.log(colors["red"]);
+console.log(colors["golden red"]);
