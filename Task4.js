850a10c029434bf62c7ae9340f806a32df7b70c4
samin710 <smsaminii112@gmail.com>
2025-02-11 21:50:51 +0600

    Task4 done

---
+++ Task4.js
@@ -0,0 +1,10 @@
+// Count the number of properties.
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+console.log(Object.keys(student).length);
+console.log(Object.values(student).length)
